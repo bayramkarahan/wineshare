9fc52b63cab37e5cd6dbebd77890edbf1bc71dca
bayramkarahan <bayramk@gmail.com>
2026-03-10 13:12:50 +0300

    wine install

--- wineshare/usr/bin/wineshare.sh
+++ wineshare/usr/bin/wineshare.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
+WINEINIT="/opt/windows"
 WINELINK="$HOME/.wine"
-#-------------------------------------------------
-#-------------------------------------------------
-/usr/bin/winechown $USER /opt/windows
-#-------------wine extension------------------------------------
-rm $HOME/.local/share/applications/wine-extension*
-#-------------------------------------------------
+WINEMONO="/opt/windows/drive_c/windows/mono"
 
+#-------------------------------------------------
+#wine init
+#-------------------------------------------------
+if [ -d $WINEINIT ]; then
+	echo "dizin var"
+else
+	echo "dizin yok"
+	WINEARCH=win64 WINEPREFIX=/opt/windows wineboot --init
+fi
+/usr/bin/winechown $USER /opt/windows
+
+#-------------wine extension----------------------
+rm $HOME/.local/share/applications/wine-extension*
+
+#-------------------------------------------------
+# wine link
+#-------------------------------------------------
 if [ -f $WINELINK ]; then
 	echo "dosya var"
 	ln -sf /opt/windows .wine
-	#cp -rf /opt/windows /tmp/
 else
 	ln -sf /opt/windows .wine
-	#cp -rf /opt/windows	/tmp/
 fi
+
 #-------------------------------------------------
+# mono install
 #-------------------------------------------------
+if [ -d $WINEMONO ]; then
+	echo "dosya var"
+else
+	echo "dosya yok"
+	wine msiexec /i /opt/wine-mono-9.0.0-x86.msi
+fi
