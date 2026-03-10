f300b0e622bfd6de284c882ea97b1fbdfc1f7f9f
bayram <bayramk@gmail.com>
2026-03-10 16:50:41 +0300

    test

--- wineshare/usr/bin/wineshare.sh
+++ wineshare/usr/bin/wineshare.sh
@@ -10,7 +10,7 @@ if [ -d $WINEINIT ]; then
 	echo "dizin var"
 else
 	echo "dizin yok"
-	WINEARCH=win64 WINEPREFIX=/opt/windows wineboot --init
+	#WINEARCH=win64 WINEPREFIX=/opt/windows wineboot --init
 fi
 /usr/bin/winechown $USER /opt/windows
 
